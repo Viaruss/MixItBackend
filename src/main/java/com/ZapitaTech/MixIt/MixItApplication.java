c1494a3f4b90d242c73cdce17660eac2ddfb5248
Viaruss <bjartoszewicz@gmail.com>
2024-04-16 17:17:17 +0200

    optimize imports in main class

--- src/main/java/com/ZapitaTech/MixIt/MixItApplication.java
+++ src/main/java/com/ZapitaTech/MixIt/MixItApplication.java
@@ -1,15 +1,11 @@
 package com.ZapitaTech.MixIt;
 
-import com.ZapitaTech.MixIt.Recipes.Flavor;
-import com.ZapitaTech.MixIt.Recipes.Recipe;
 import com.ZapitaTech.MixIt.Recipes.RecipeDAO;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
